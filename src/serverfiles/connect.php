560bf22d863096c81d793c0d46b8eaff37c81b04
kyle reich <kylereich@kyles-mbp.lan>
2019-12-23 10:02:56 -0700

    moved DB to my own RDS instance

--- src/serverfiles/connect.php
+++ src/serverfiles/connect.php
@@ -1,6 +1,9 @@
 <?php
-    $dsn = 'mysql:host=bluemedia1.cism30bqpd7x.us-west-1.rds.amazonaws.com;dbname=connect';
-    $username = 'bluemedia';
-    $password = 'egrant1385';
+    //$dsn = 'mysql:host=bluemedia1.cism30bqpd7x.us-west-1.rds.amazonaws.com;dbname=connect';
+    //$username = 'bluemedia';
+    //$password = 'egrant1385';
+    $dsn = 'mysql:host=database-2.ci2fm22a4hms.us-west-2.rds.amazonaws.com;dbname=innodb';
+    $username = 'admin';
+    $password = 'Egrant1385';
     $dbh = new PDO($dsn, $username, $password);
 ?>
